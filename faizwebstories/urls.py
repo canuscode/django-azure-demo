0c05d76583dd4c624cf6dcdfd28bdf4f3a4ed7dd
imuser <imuser@hotmail.com>
2020-02-18 07:22:20 -0800

    Fixed Homepage

--- faizwebstories/urls.py
+++ faizwebstories/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stories/', include('stories.urls'))
+    path('', include('stories.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
